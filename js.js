14e9f6398e759037a0f22c08beeaf38346d4a265
Jim Moroco <jim.moroco@gmail.com>
2022-08-08 21:41:40 -0500

    add html css js

--- js.js
+++ js.js
@@ -3,17 +3,29 @@ let sideOfSmallSquares = 9
 window.onload = function () {
     generateTemplate();
     resize();
+    //sizes();
 }
 window.onresize = resize;
 
 function resize() {
     const width = document.querySelector(".small-square").offsetWidth;
-    // const height = document.querySelector(".small-square").offsetHeight;
+
+    const top = document.querySelector(".top");
+    top.style.setProperty("height", `${window.innerHeight * 0.2}px`);
+
     const grid = document.querySelector(".grid");
     grid.style.setProperty("width", `${width * sideOfSmallSquares}px`);
-    // grid.style.setProperty("grid-auto-rows", `${width}px`);
-    // grid.style.setProperty("grid-auto-columns", `${height}px`);
     grid.style.setProperty("grid-template-columns", `repeat(auto-fit, ${100 / sideOfSmallSquares}%)`);
+
+    const bottom = document.querySelector(".bottom");
+    bottom.style.setProperty("height", `${window.innerHeight * 0.2}px`);
+
+
+
+    //const container = document.querySelector(".container");
+    // container.style.setProperty("grid-template-rows", `50% ${width * sideOfSmallSquares}`); // : 20% 60% 20% ;");
+    //sizes();
+
 }
 
 function generateTemplate() {
@@ -27,4 +39,23 @@ function getSmallSquare() {
     square.classList.add("small-square");
     square.innerText = "X"
     return square;
+}
+
+
+function sizes() {
+    var screenWidth = screen.width;
+    var screenHeight = screen.height;
+    // Get the browser window size
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    // Get the size of the entire webpage
+    const pageWidth = document.documentElement.scrollWidth;
+    const pageHeight = document.documentElement.scrollHeight;
+    // Showing the sizes on the webpage
+    var x = document.getElementById("screen");
+    x.innerHTML = "Device Screen: width: " + screenWidth + ", height: " + screenHeight + ".";
+    var y = document.getElementById("window");
+    y.innerHTML = "Browser Window: width: " + windowWidth + ", height: " + windowHeight + ".";
+    var z = document.getElementById("page");
+    z.innerHTML = "Webpage: width:" + pageWidth + ", height: " + pageHeight + ".";
 }
